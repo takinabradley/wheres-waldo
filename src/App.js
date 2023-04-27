2ca7f4c1392622029bd1f59fbda6c86da77496b3
takinabradley <bradfinney98@gmail.com>
2023-04-26 23:37:36 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,4 @@
+export default function App() {
+
+  return <div className="app">Welcome to my React App</div>
+}
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+const isProduction = process.env.NODE_ENV == "production"
+
+const stylesHandler = isProduction
+  ? MiniCssExtractPlugin.loader
+  : "style-loader"
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "main.js"
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+    static: ["./public"],
+    hot: true,
+    liveReload: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html"
+    })
+
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  target: "web",
+  resolve: {
+    /** "extensions"
+     * If multiple files share the same name but have different extensions, webpack will
+     * resolve the one with the extension listed first in the array and skip the rest.
+     * This is what enables users to leave off the extension when importing
+     */
+    extensions: [".js", ".jsx", ".json"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        exclude: /node_modules/, //folder to be excluded
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader"]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [stylesHandler, "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset"
+      }
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ]
+  }
+}
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production"
+
+    config.plugins.push(new MiniCssExtractPlugin())
+  } else {
+    config.mode = "development"
+  }
+  return config
+}
